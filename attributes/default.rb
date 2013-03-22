9c58087395e0d7014eacb06f7368b6d09fcdd4bc
phumpal <patrick@netvilla.net>
2013-03-22 13:06:30 -0700

    modified attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,7 +24,7 @@ default[:sugarcrm][:db][:user] = 'sugarcrm'
 default[:sugarcrm][:dir] = 'sugarcrm'
 default[:sugarcrm][:admin_pass] = 'admin'
 
-default[:sugarcrm][:webroot] = "/var/www/#{node[:sugarcrm][:dir]}"
+default[:sugarcrm][:webroot] = "/var/www/#{default[:sugarcrm][:dir]}"
 
 ::Chef::Node.send(:include, Opscode::OpenSSL::Password)
 
