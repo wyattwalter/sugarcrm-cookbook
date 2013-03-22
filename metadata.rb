3dddbd353258a6b378ea3a03ae93473cb93fba1e
phumpal <patrick@netvilla.net>
2013-03-22 13:32:30 -0700

    Apache template

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,9 @@ maintainer_email "wwalter@sugarcrm.com"
 license          "Apache 2.0"
 description      "Installs/Configures SugarCRM CE"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.3"
+version          "0.1.4"
+
+supports "ubuntu"
 
 depends "apt"
 depends "build-essential"
