a14b5c0890281974f7246a595fca1745b8c1f18f
Christian Fischer <sw-dev@computerlyrik.de>
2014-08-20 00:17:45 +0200

    upgrade to version 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures SugarCRM CE"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 supports 'ubuntu'
 
