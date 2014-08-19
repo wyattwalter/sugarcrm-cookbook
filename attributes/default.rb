fa12ebc6e2cc93fca1c70fd0c4526ae21ccbaf93
Christian Fischer <sw-dev@computerlyrik.de>
2014-08-19 20:27:37 +0200

    foodcritic fixups

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,12 +22,12 @@
 
 default['sugarcrm']['version'] = nil # "6.5.16"
 
-default[:sugarcrm][:db][:hostname] = 'localhost'
-default[:sugarcrm][:db][:name] = 'sugarcrm'
-default[:sugarcrm][:db][:user] = 'sugarcrm'
-default[:sugarcrm][:db][:password] = secure_password
+default['sugarcrm']['db']['hostname'] = 'localhost'
+default['sugarcrm']['db']['name'] = 'sugarcrm'
+default['sugarcrm']['db']['user'] = 'sugarcrm'
+default['sugarcrm']['db']['password'] = secure_password
 
-default[:sugarcrm][:admin_pass] = secure_password
+default['sugarcrm']['admin_pass'] = secure_password
 
-default[:sugarcrm][:dir] = 'sugarcrm'
-default[:sugarcrm][:webroot] = "#{node[:apache][:docroot_dir]}/#{node[:sugarcrm][:dir]}"
+default['sugarcrm']['dir'] = 'sugarcrm'
+default['sugarcrm']['webroot'] = "#{node[apache][docroot_dir]}/#{node[sugarcrm][dir]}"
